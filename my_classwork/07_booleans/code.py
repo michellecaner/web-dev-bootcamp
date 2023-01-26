b7c2f6cccf8569c944f241618874b80057e03f28
michellecaner <michellecaner@gmail.com>
2023-01-26 14:55:40 -0600

    boolean refresher

--- my_classwork/07_booleans/code.py
+++ my_classwork/07_booleans/code.py
@@ -1,1 +1,10 @@
 """Python refresher course - Booleans Section"""
+
+# are these exactly equal?
+print(5 == 5)
+
+# is one greater than the other?
+print(5 > 5)
+
+# is one different from the other?
+print(10 != 10)
