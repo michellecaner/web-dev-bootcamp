309cded222bffd78552f91e1c184627b9957d73a
michellecaner <michellecaner@gmail.com>
2023-01-30 17:40:35 -0600

    kwargs refresher

---
+++ my_classwork/22_unpacking_keyword_args/code.py
@@ -0,0 +1,19 @@
+"""Python refresher course - Unpacking Keyword Arguments Section"""
+
+# kwargs = keyword arguments
+def named(**kwargs): # ** collects arguments
+    print(kwargs)    # {'name': 'Jo', 'age': 75}
+
+named(name="Jo", age=75) # two keyword arguments that get collected in the kwarg variable & results in a dictionary
+
+
+
+# so confusing
+def bio(name, age):
+    print(name, age)
+
+details = {"name": "Al", "age": 85}
+
+bio(**details) # ** unpacks dictionary
+
+
