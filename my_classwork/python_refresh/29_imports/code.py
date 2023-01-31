6b03e525fe16586bf39fede90f256349cc275742
michellecaner <michellecaner@gmail.com>
2023-01-31 15:04:04 -0600

    imports refresher

---
+++ my_classwork/python_refresh/29_imports/code.py
@@ -0,0 +1,8 @@
+"""Python refresher course - Importing Section"""
+
+import sys
+from mymodule import divide
+
+print(divide(10, 2))
+
+print(sys.path) # tells us where python is looking to import things from
---
+++ my_classwork/python_refresh/29_imports/mymodule.py
@@ -0,0 +1,12 @@
+"""Python refresher course - Importing Section"""
+
+import libs.mylib
+
+def divide(dividend, divisor):
+    return dividend / divisor
+
+# __name__ is a global variable that changes depending what file you're in
+print("mymodule.py ", __name__) # mymodule.py  __main__ <-- the main function in the module?
+
+
+
