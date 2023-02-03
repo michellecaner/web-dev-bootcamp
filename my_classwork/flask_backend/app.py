94f3137e4fc6a1b982e473ce8dc69c6ee64fe25c
michellecaner <michellecaner@gmail.com>
2023-02-02 18:06:30 -0600

    hello world app

--- my_classwork/flask_backend/app.py
+++ my_classwork/flask_backend/app.py
@@ -7,3 +7,17 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+  
+  
+@app.route("/fancy")
+def hello_world_fancy():
+    return """
+    <html>
+    <body>
+    
+    <h1>Greetings!</h1>
+    <p>Hello, world!</p>
+    
+    </body>
+    </html>
+  """
