2d204a1ef98fbe2ea7887914e03288b19f8e9f77
michellecaner <michellecaner@gmail.com>
2023-02-02 18:57:04 -0600

    routing to html page templates

--- my_classwork/flask_backend/app.py
+++ my_classwork/flask_backend/app.py
@@ -1,23 +1,15 @@
 # app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template("first_page.html")
+# render_template is a built in function that takes the name of a file to load up & send to the user
 
 
-@app.route("/fancy")
+@app.route("/second")
 def hello_world_fancy():
-    return """
-    <html>
-    <body>
-    
-    <h1>Greetings!</h1>
-    <p>Hello, world!</p>
-    
-    </body>
-    </html>
-"""
+    return render_template("second_page.html")
