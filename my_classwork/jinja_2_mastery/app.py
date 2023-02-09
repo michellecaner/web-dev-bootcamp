2749d1118156c62afb536b7dc163fe44dd92caa6
michellecaner <michellecaner@gmail.com>
2023-02-09 17:22:48 -0600

    first go at jinja2 macros

--- my_classwork/jinja_2_mastery/app.py
+++ my_classwork/jinja_2_mastery/app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# values below are for variables titled: text & completed 
+todos = [
+    ("Get milk", False),
+    ("Learn code", True)
+]
 
 @app.route("/")
 def todo():
-    return render_template("home.html", todos=["Get milk", "Learn programming"])
+    return render_template("home.html", todos=todos)
