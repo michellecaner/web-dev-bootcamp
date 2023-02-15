b656c26a802dc990be456dba3fe9156be91b17cf
michellecaner <michellecaner@gmail.com>
2023-02-15 09:50:25 -0600

    roadblock with connecting to MongoDB - stopping point

--- my_classwork/habit_tracker/app.py
+++ my_classwork/habit_tracker/app.py
@@ -1,9 +1,17 @@
+import os
 from flask import Flask
 from routes import pages
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # creates app factory
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(pages)
- 
+    client = MongoClient(os.environ.get("MongoDB_URI"))
+    app.db = client.get_default_database()
+
     return app
+
--- my_classwork/habit_tracker/routes.py
+++ my_classwork/habit_tracker/routes.py
@@ -1,60 +1,78 @@
 import datetime
-from collections import defaultdict
-from flask import Blueprint, render_template, request, url_for, redirect
+import uuid
+from flask import Blueprint, current_app, render_template, request, url_for, redirect
 
 
-pages = Blueprint("habits", __name__, template_folder="templates", static_folder="static")
-habits = ["Test habit"]
-completions = defaultdict(list) # creating default dict
+pages = Blueprint(
+    "habits", __name__, template_folder="templates", static_folder="static"
+    )
 
 
 # func using list comp to get starting/current date & to get dates 3 days before & after
 # timedelta is adding 3 or subtracting 3 days from the starting/current date
 @pages.context_processor
 def add_calc_date_range():
-    def date_range(start: datetime.date):
+    def date_range(start: datetime.datetime):
         dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
         return dates
 
     return {"date_range": date_range}
 
+
+def today_at_midnight():
+    today = datetime.datetime.today()
+    return datetime.datetime(today.year, today.month, today.day)
+
+
 @pages.route("/")
 def index():
     date_str =request.args.get("date")
     if date_str:
-        selected_date = datetime.date.fromisoformat(date_str)
+        selected_date = datetime.datetime.fromisoformat(date_str)
     else:
-        selected_date = datetime.date.today()
+        selected_date = today_at_midnight()
+
+    habits_on_date = current_app.db.habits.find({"added": {"$lte": selected_date}})
+
+    completions =  [
+        habit["habit"]
+        for habit in current_app.db.completions.find({"date": selected_date})
+    ]
 
     return render_template(
         "index.html", 
-        habits = habits,
+        habits = habits_on_date,
         selected_date=selected_date,
-        completions=completions[selected_date],
+        completions=completions,
         title="Habit Tracker - Home"
         )
 
 
 @pages.route("/add", methods=["GET", "POST"])
 def add_habit():
-    if request.method == "POST":
-        habit = request.form.get("habit") # <--getting habit user input
-        habits.append(habit) # <--appending to the list
+    today = today_at_midnight()
+
+    if request.form:
+      current_app.db.habits.insert_one(
+          {"_id": uuid.uuid4().hex, "added": today, "name": request.form.get("habit")}
+      )
+
     return render_template(
         "add_habit.html", 
-        title="Habit Tracker - Add Habit", 
-        selected_date=datetime.date.today()
+        title="Habit Tracker - Add Habit",
+        selected_date=today
     )
+    
+    
 # new flask feature
 # instead of @app.route("/complete", methods=["POST"])
 # need to make a place for that completed habit data to go
 # @app.post("/complete")
-
 @pages.route("/complete", methods=["POST"])
 def complete():
-    date_string = request.form.get("date") # <--names of the fields
-    habit = request.form.get("habitName") # <--names of the fields
-    date = datetime.date.fromisoformat(date_string)
-    completions[date].append(habit)
+    date_string = request.form.get("date") # <--db key
+    habit = request.form.get("habitId") # <--db key
+    date = datetime.datetime.fromisoformat(date_string)
+    current_app.db.completions.insert_one({"date": date, "habit": habit})
 
     return redirect(url_for(".index", date=date_string))
