2befc8d4c368c6cdf02159f25f74aa0c28eb864c
michellecaner <michellecaner@gmail.com>
2023-02-28 19:55:39 -0600

    added movie model w/ default fields

--- my_classwork/movie_library_project/movie_library/routes.py
+++ my_classwork/movie_library_project/movie_library/routes.py
@@ -1,10 +1,17 @@
+import uuid
 from flask import (
-    Blueprint, 
+    Blueprint,
     redirect,
     render_template,
+    url_for,
     session,
-    request
+    request,
+    current_app
 )
+
+from dataclasses import asdict
+from movie_library.forms import MovieForm
+from movie_library.models import Movie
 
 pages = Blueprint(
     "pages", __name__, template_folder="templates", static_folder="static"
@@ -16,6 +23,29 @@ def index():
     return render_template(
         "index.html",
         title="Movies Watchlist",
+    )
+
+
+@pages.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = MovieForm()
+
+    if form.validate_on_submit():
+        movie = Movie(
+            _id=uuid.uuid4().hex,
+            title=form.title.data,
+            director=form.director.data,
+            year=form.year.data
+        )
+
+        current_app.db.movie.insert_one(asdict(movie))
+
+        return redirect(url_for(".index"))
+
+    return render_template(
+        "new_movie.html",
+        title="Movies Watchlist - Add Movie",
+        form=form
     )
 
 
