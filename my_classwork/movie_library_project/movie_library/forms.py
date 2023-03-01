998d79aba06ffe821b1aabc97aa72be15b6e197b
michellecaner <michellecaner@gmail.com>
2023-03-01 15:09:24 -0600

    set up user login

--- my_classwork/movie_library_project/movie_library/forms.py
+++ my_classwork/movie_library_project/movie_library/forms.py
@@ -112,3 +112,9 @@ class RegisterForm(FlaskForm):
     )
 
     submit = SubmitField("Register")
+    
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[InputRequired(), Email()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Login")
--- my_classwork/movie_library_project/movie_library/routes.py
+++ my_classwork/movie_library_project/movie_library/routes.py
@@ -16,6 +16,7 @@ from movie_library.forms import (
     MovieForm,
     ExtendedMovieForm,
     RegisterForm,
+    LoginForm
 )
 from movie_library.models import Movie, User
 
@@ -57,13 +58,38 @@ def register():
 
         flash("User registered successfully", "success")
 
-        return redirect(url_for(".index"))
+        return redirect(url_for(".login"))
 
     return render_template(
         "register.html", 
         title="Movies Watchlist - Register", 
         form=form
     )
+
+
+@pages.route("/login", methods=["GET", "POST"])
+def login():
+    if session.get("email"):
+        return redirect(url_for(".index"))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user_data = current_app.db.user.find_one({"email": form.email.data})
+        if not user_data:
+            flash("Login credentials not correct", category="danger")
+            return redirect(url_for(".login"))
+        user = User(**user_data)
+
+        if user and pbkdf2_sha256.verify(form.password.data, user.password):
+            session["user_id"] = user._id
+            session["email"] = user.email
+
+            return redirect(url_for(".index"))
+
+        flash("Login credentials not correct", category="danger")
+
+    return render_template("login.html", title="Movies Watchlist - Login", form=form)
 
 
 @pages.route("/add", methods=["GET", "POST"])
