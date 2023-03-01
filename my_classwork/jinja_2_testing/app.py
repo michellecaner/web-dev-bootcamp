4fbb043c9b1d16609602e79fea82a6ae45310237
michellecaner <michellecaner@gmail.com>
2023-03-01 16:22:28 -0600

    fixed formatting

--- my_classwork/jinja_2_testing/app.py
+++ my_classwork/jinja_2_testing/app.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 # writing our own tests
 # this one passes in a value, gets the square root & asks if an int
 def square(value):
     return (value ** 0.5).is_integer()
 
 
-app.jinja_env.tests["square"] = square
+# app.jinja_env.tests["square"] = square
 # then you put it in the jinja env & makes it avail in every template
 
 @app.route("/")
